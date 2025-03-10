732e2ebf0246e8d0ffa36c7cacdfbea6b23ca522
vinayfeelme <109200130+vinayfeelme@users.noreply.github.com>
2025-03-10 15:33:10 +0530

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-
 # This script prints a greeting message.
-
 echo "Hello, World!"
 echo "Today is: $(date)"
-echo "Your current working directory is: $(pwd)"
-
-# A simple if-else statement
-if [ -d "$1" ]; then
-    echo "Directory $1 exists."
-else
-    echo "Directory $1 does not exist."
-fi
